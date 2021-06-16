1877e1e93a565677d22ca158c6ebd60fc99ab421
rossioctave <octave.rossi@gmail.com>
2021-06-16 10:34:15 +0200

    v0.3.1 +1st terrain buggy

--- src/index.js
+++ src/index.js
@@ -27,12 +27,76 @@ const renderWindow = fullScreenRenderer.getRenderWindow();
 // Example code
 // ----------------------------------------------------------------------------
 
-const planeSource = vtkPlaneSource.newInstance();
+// const planeSource = vtkPlaneSource.newInstance();
 
+const SIZE_MAP_X = 3;
+const SIZE_MAP_Y = 3;
+const SIZE_MAP_Z = 0;
 
-function fillPolyData() {
-	var polyData  = vtkPolyData.newInstance();
-	var nbPoints = 3 * 1;
+const polyData  = vtkPolyData.newInstance();
+
+// place les points
+function calc_map() {
+	let nbPoints = (SIZE_MAP_X + 1) * (SIZE_MAP_Y + 1);
+	let numTriangles = SIZE_MAP_X * SIZE_MAP_Y * 2;
+
+	let points = new Float64Array(nbPoints * 3);
+	polyData.getPoints().setData(points, 3);
+
+	let polys = new Uint32Array(4 * numTriangles);
+	polyData.getPolys().setData(polys, 1);
+
+	// TO DO : set normals?
+	let idx = 0;
+	for (let i = 0; i <= SIZE_MAP_X; i++) {
+		for (let j = 0; j <= SIZE_MAP_Y; j++) {
+			points[idx * 3] = SIZE_MAP_X - i;
+			points[idx * 3 + 1] = SIZE_MAP_Y - j;
+			points[idx * 3 + 2] = 0; // z coords
+			idx++;
+		}
+	}
+
+	idx = 0;
+	/*
+	for (let k = 0; k < SIZE_MAP_X; k++) {
+		for (let l = 0; l < SIZE_MAP_Y; l++) {
+			polys[idx * 4] = 3;
+			polys[idx * 4 + 1] = k + l * (SIZE_MAP_X + 1); // ref point for triangle
+			polys[idx * 4 + 2] = 
+				(k + l % 2 === 0)?
+				polys[idx * 4 + 1] + 1 : //	right point in grid
+				polys[idx * 4 + 1] + SIZE_MAP_X + 1; // 1 row below point in grid
+			polys[idx * 4 + 3] = 
+				(k + l % 2 === 0)?
+				polys[idx * 4 + 1] + SIZE_MAP_X + 1 :// 1 row below point in grid
+				polys[idx * 4 + 1] + SIZE_MAP_X - 1 ;// one point below & 1 left in grid
+
+			idx++;
+		}
+	}
+	*/
+
+	for (let k = 0; k < SIZE_MAP_X; k++) {
+		for (let l = 0; l < SIZE_MAP_Y; l++) {
+			polys[idx * 4] = 3;
+			polys[idx * 4 + 1] = k * (SIZE_MAP_X) + l; // ref point for triangle
+			polys[idx * 4 + 2] = 
+				(k + l % 2 === 0)?
+				polys[idx * 4 + 1] + 1 : //	right point in grid
+				polys[idx * 4 + 1] + SIZE_MAP_X; // 1 row below point in grid
+			polys[idx * 4 + 3] = 
+				(k + l % 2 === 0)?
+				polys[idx * 4 + 1] + SIZE_MAP_X:// 1 row below point in grid
+				polys[idx * 4 + 1] + SIZE_MAP_X - 1;// one point below & 1 left in grid
+
+			idx++;
+		}
+	}
+}
+
+function create_triangle() {
+	var nbPoints = 4 * 1;
 	var points = new Float32Array(nbPoints * 3);
 
 
@@ -56,11 +120,9 @@ function fillPolyData() {
 
 	polyData.getPoints().setData(points, 3);
   polyData.getPolys().setData(cells, 1);
-
-	return polyData;
 }
 
-const polyData = fillPolyData();
+calc_map();
 
 
 
@@ -81,6 +143,10 @@ renderer.addActor(actor);
 renderer.resetCamera();
 renderWindow.render();
 
+
+global.renderWindow = renderWindow;
+global.fullScreenRenderer = fullScreenRenderer;
+global.renderer = renderer;
 global.polyData = polyData;
 global.planeSource = planeSource;
 console.log("hoho");
