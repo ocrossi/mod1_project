f34172be96db60c4437a3ced86f6d810c750ebc5
Octave Rossi <ocrossi@z3r10p5.42lyon.fr>
2021-06-17 13:47:33 +0200

    v0.3.3 ~webpack still blurry

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,20 @@
 var path = require('path');
 
 module.exports = {
-  //...
+  
+	mode: "development",
+	entry: './src/index.js',
+	output: {
+		filename: 'output.js',
+		path: path.resolve(__dirname, "test")
+	},
+	
+	//...
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
   },
-	/*
-	output: {
-		filename: 'output.js',
-		path: path.resolve(__dirname, "dist")
-	}
-	*/
+
+	
 };
