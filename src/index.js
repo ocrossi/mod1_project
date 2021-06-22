9b08eff36f4b5987355675f663e02265e1363a37
Octave Rossi <ocrossi@z3r10p1.42lyon.fr>
2021-06-22 18:14:30 +0200

    v0.3.5 +parse_input beginning
    next time find good regez to match each points

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-import '@kitware/vtk.js/Rendering/Profiles/Geometry';
-import '@kitware/vtk.js/macro.js';
+import "@kitware/vtk.js/Rendering/Profiles/Geometry";
+import "@kitware/vtk.js/macro.js";
 
-import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
+import vtkFullScreenRenderWindow from "@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow";
+import vtkActor from "@kitware/vtk.js/Rendering/Core/Actor";
+import vtkMapper from "@kitware/vtk.js/Rendering/Core/Mapper";
+import vtkCalculator from "@kitware/vtk.js/Filters/General/Calculator";
+import vtkPolyData from "@kitware/vtk.js/Common/DataModel/PolyData.js";
+import vtkPlaneSource from "@kitware/vtk.js/Filters/Sources/PlaneSource";
+import { Representation } from "@kitware/vtk.js/Rendering/Core/Property/Constants";
 
-import vtkActor           from '@kitware/vtk.js/Rendering/Core/Actor';
-import vtkMapper          from '@kitware/vtk.js/Rendering/Core/Mapper';
-import vtkCalculator      from '@kitware/vtk.js/Filters/General/Calculator';
-
-import vtkPolyData from '@kitware/vtk.js/Common/DataModel/PolyData.js'
-
-
-import vtkPlaneSource from '@kitware/vtk.js/Filters/Sources/PlaneSource';
-import { Representation } from '@kitware/vtk.js/Rendering/Core/Property/Constants';
-
-import inputFile from 'raw-loader!../resources/demo1.mod1';
-import controlPanel from './controlPanel.html';
+import inputFile from "raw-loader!../resources/demo5.mod1";
+import controlPanel from "./controlPanel.html";
 
 // ----------------------------------------------------------------------------
 // Standard rendering code setup
@@ -28,34 +24,49 @@ const renderWindow = fullScreenRenderer.getRenderWindow();
 // Example code
 // ----------------------------------------------------------------------------
 
-const SIZE_MAP_X = 8;
-const SIZE_MAP_Y = 8;
+const SIZE_MAP_X = 100;
+const SIZE_MAP_Y = 100;
 const SIZE_MAP_Z = 0;
 
-const polyData  = vtkPolyData.newInstance();
+const polyData = vtkPolyData.newInstance();
 
-var inputName = 'demo1';
-
+/* tests for custom import hard coded
+var inputName = "demo1";
 async function importModule() {
-
-  import(`raw-loader!../resources/${inputName}.mod1`).then((test) => {
-    console.log("Loaded");
-		console.log(test.default);
-  }, (err)=>{
-    console.log("Error", err)
-  })
+	const ret = await import(`raw-loader!../resources/${inputName}.mod1`);
+	return ret;
 }
+*/
 
 function parse_input() {
-	console.log('input|', inputFile, '|EOI');
+	const reg1 = new RegExp('".*"');
+	const t1 = reg1.exec(inputFile); // prend tout ce qui est entre guillemets
+	const t2 = t1[0].substr(1, t1[0].length - 2); // enleve 1er et dernier char : les guillemets
+
+	console.log("input une fois traite pour ne garder que le content du file :");
+	console.log(t2);
+
+
+	const reg2 = new RegExp("^\([0-9]+,[0-9]+,[0-9]+\)$");
+
+	let lineTab = t2.split("\\n");
+	console.log(lineTab);
+	for (let i = 0; i < lineTab.length; i++) {
+		//console.log("lines : ", lineTab[i]);
+		let pointsTab = lineTab[i].split(" ");
+		//console.log("pointsTab", pointsTab);
+		for (let j = 0; j < pointsTab.length - 1; j++) {
+			if (pointsTab[j].match(reg2) !== null) console.log("match");
+			else {
+				console.error("STOP RIGHT THERE");
+				console.log(pointsTab[j]);
+			}
+		}
+	}
 }
 
-function calc_map_size() {
-	
-}
+function calc_map_size() {}
 
-
-// for now generates flat squared map
 function generate_map() {
 	let nbPoints = (SIZE_MAP_X + 1) * (SIZE_MAP_Y + 1);
 	let numTriangles = SIZE_MAP_X * SIZE_MAP_Y * 2;
@@ -66,8 +77,6 @@ function generate_map() {
 	let polys = new Uint32Array(4 * numTriangles);
 	polyData.getPolys().setData(polys, 1);
 
-	// TO DO : set normals?
-	
 	let idx = 0;
 	for (let i = 0; i <= SIZE_MAP_X; i++) {
 		for (let j = 0; j <= SIZE_MAP_Y; j++) {
@@ -81,7 +90,7 @@ function generate_map() {
 	idx = 0;
 
 	for (let k = 0; k < SIZE_MAP_X; k++) {
-		for (let l = 0; l < SIZE_MAP_Y; l++) {	
+		for (let l = 0; l < SIZE_MAP_Y; l++) {
 			polys[idx * 8] = 3;
 			polys[idx * 8 + 1] = k * (SIZE_MAP_X + 1) + l; // ref
 			polys[idx * 8 + 2] = polys[idx * 8 + 1] + 1; // down
@@ -89,49 +98,20 @@ function generate_map() {
 
 			polys[idx * 8 + 4] = 3;
 			polys[idx * 8 + 5] = polys[idx * 8 + 2];
-			polys[idx * 8 + 6] = polys[idx * 8 + 3]; 
-			polys[idx * 8 + 7] = polys[idx * 8 + 3] + 1; 
+			polys[idx * 8 + 6] = polys[idx * 8 + 3];
+			polys[idx * 8 + 7] = polys[idx * 8 + 3] + 1;
 			idx++;
 		}
 	}
 }
 
 function main() {
-	/* tests */
-
-	importModule();
-	console.log('1');
-
-	/* code */
-	generate_map();
 	parse_input();
-	console.log('2');
-
+	//calc_map_size();
+	generate_map();
 }
 
 main();
-
-// -----------------------------------------------------------
-// UI control handling
-// -----------------------------------------------------------
-
-fullScreenRenderer.addController(controlPanel);
-
-function read_input() {
-	const reader = new FileReader();
-	reader.onload = function() {
-		console.log(reader.result);
-	}
-	reader.readAsText(input.files[0]);
-}
-
-
-
-const input = document.querySelector('input[type="file"]');
-input.addEventListener('change', (e) => {
-	console.log(input.files);
-	reader.onload = () => console.log(reader.result);
-}, false);
 
 // ----------------------------------------------------------------------------
 // Display output
@@ -139,8 +119,6 @@ input.addEventListener('change', (e) => {
 
 const mapper = vtkMapper.newInstance();
 mapper.setInputData(polyData);
-
-
 
 const actor = vtkActor.newInstance();
 actor.getProperty().setRepresentation(Representation.WIREFRAME);
@@ -150,7 +128,6 @@ actor.setMapper(mapper);
 renderer.addActor(actor);
 renderer.resetCamera();
 renderWindow.render();
-
 
 global.renderWindow = renderWindow;
 global.fullScreenRenderer = fullScreenRenderer;
